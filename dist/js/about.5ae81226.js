5a8f4863c7a9dbf2cd58f1474c3244ca2a7c7f1c
Urguxxi <shannonma84@gmail.com>
2024-08-14 13:14:21 +0200

    Modified the config file deleted dist dir

--- dist/js/about.5ae81226.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[594],{2658:function(n,e,t){t.r(e),t.d(e,{default:function(){return o}});var u=t(6768);const a={class:"about"},c=(0,u.Lk)("h1",null,"This is an about page hehe",-1),r=[c];function s(n,e){return(0,u.uX)(),(0,u.CE)("div",a,r)}var i=t(1241);const l={},h=(0,i.A)(l,[["render",s]]);var o=h}}]);
-//# sourceMappingURL=about.5ae81226.js.map
